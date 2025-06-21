27b66fe3cd738dac9c5fa11754d2d62896c3302a
ADITYA SAH <134919531+adityasah104@users.noreply.github.com>
2025-06-21 13:42:07 +0530

    Update push_to_docker_hub.groovy

--- vars/push_to_docker_hub.groovy
+++ vars/push_to_docker_hub.groovy
@@ -2,5 +2,5 @@ def call(String Project,String ImageTag,String dockerhubuser){
   withCredentials([usernamePassword('credentialsId':"dockerHubCred",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
-    sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+    sh "docker push ${dockerhubuser}/${Project}:${ImageTag}"
 }
